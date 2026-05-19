dc69439ac4ee0c53a3ab7848e823f142b3a72fd6
ecam-b <ec.angaritas@gmail.com>
2026-05-18 19:06:39 -0500

    feat/ Change idea. Create first all entities, models and repositoryInterfaces.

--- app/domain/repositories/transaction_repository.py
+++ app/domain/repositories/transaction_repository.py
@@ -6,14 +6,18 @@ class TransactionRepositoryInterface(ABC):
     def save(self, transaction: Transaction) -> Transaction:
         pass
 
+    @abstractmethod
     def get_all(self, offset: int, limit: int) -> list[Transaction]:
         pass
 
+    @abstractmethod
     def get_by_id(self, id: int) -> Transaction:
         pass
 
+    @abstractmethod
     def update(self, id: int, transaction: Transaction) -> Transaction:
         pass
 
+    @abstractmethod
     def delete(self, id: int) -> dict:
         pass
