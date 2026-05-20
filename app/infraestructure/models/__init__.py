8360937dfff4b39cc8286b8f5a9282cf2a1f917d
ecam-b <ec.angaritas@gmail.com>
2026-05-20 15:48:55 -0500

    feat/ Migration to Postgresql success. Add compose.yaml to manage db on docker.

---
+++ app/infraestructure/models/__init__.py
@@ -0,0 +1,7 @@
+from sqlmodel import SQLModel
+
+from .transaction import TransactionTable
+from .category import CategoryTable
+from .account import AccountTable
+from .obligation import ObligationTable
+from .debt import DebtTable
