b44f57d6f9602b2d5ce1f7de6b91837562a21fd7
ecam-b <ec.angaritas@gmail.com>
2026-05-29 11:45:00 -0500

    feat: basic front web with jinja create.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 # from contextlib import asynccontextmanager
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from app.presentation.api.v1.transaction_router import router as transaction_router
 from app.presentation.api.v1.category_router import router as category_router
 from app.presentation.api.v1.account_router import router as account_router
@@ -25,16 +27,10 @@ load_dotenv()
 
 configure_logger(is_production=os.getenv("PRODUCTION", False))
 
-""" 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # I need the app create db and table since start.
-    create_db_and_table()
-    yield 
-"""
-
-# app = FastAPI(lifespan=lifespan)
 app = FastAPI()
+
+templates = Jinja2Templates(directory="app/presentation/web/templates")
+app.mount("/static", StaticFiles(directory="app/presentation/web/static"), name="static")
 
 app.add_middleware(CorrelationIdMiddleware)
 
@@ -54,5 +50,5 @@ app.include_router(debt_router, prefix="/api/v1", tags=["debt"])
 app.include_router(user_router, prefix="/api/v1", tags=["user"])
 
 @app.get("/")
-def home() -> dict:
-    return {"msg": "server is connect"}
+def home(request: Request):
+    return templates.TemplateResponse(request=request, name="home.html")
